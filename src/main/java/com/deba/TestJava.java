e023d9fe3304ee64177bc6f6111ce677278da55b
debashis.poddar <debashis.poddar@gmail.com>
2022-06-26 23:56:00 +0530

    Debashis Main 4 from Eclipse

--- src/main/java/com/deba/TestJava.java
+++ src/main/java/com/deba/TestJava.java
@@ -4,7 +4,7 @@ public class TestJava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println("Debashis Main 3 ");
+			System.out.println("Debashis Main 4 from Eclipse ");
 	}
 
 }
