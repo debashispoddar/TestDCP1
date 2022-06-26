f6f83431e2940523554b9b3d54fb6bae4381c6fb
debashis.poddar <debashis.poddar@gmail.com>
2022-06-27 00:01:40 +0530

    Debashis Main 5 from Command Line

--- src/main/java/com/deba/TestJava.java
+++ src/main/java/com/deba/TestJava.java
@@ -4,7 +4,7 @@ public class TestJava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println("Debashis Main 4 from Eclipse ");
+			System.out.println("Debashis Main 5 from Command Line ");
 	}
 
 }
