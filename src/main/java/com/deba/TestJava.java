8c6a242b75e14b8c7509f102e204f9b801c5318d
DEBASHIS <DEBASHIS@user-PC>
2022-08-02 13:46:06 +0530

    2

--- src/main/java/com/deba/TestJava.java
+++ src/main/java/com/deba/TestJava.java
@@ -4,7 +4,7 @@ public class TestJava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			System.out.println("Debashis Main 6 from Eclipse ");
+			System.out.println("Debashis Main 6 from Eclipse new1");
 	}
 
 }
